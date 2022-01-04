590ff3610dca2294c7d3b90f5342f5cd78fe58f8
parwinderD4D <parwinder.d4d@gmail.com>
2022-01-04 16:03:54 +0530

    Done computed property for activeItems in vue file

---
+++ VueJs_File/Array.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="widget">
+    <div v-if="activeItems && activeItems.length > 0">
+      <ul>
+        <li v-for="item in activeItems" :key="item.id">
+          {{item.name}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        loading: true,
+        list: {
+          'John': true,
+          'Jane': true,
+          'Bob': false
+        },
+      }
+    },
+    computed: {
+      activeItems() {
+        
+		var newList  this.list;
+    
+		var active = [];
+			for (const key in newList) {
+			
+				if(list[key] == true )
+				{
+				  var obj = {name : key , active : true}
+				  active.push(obj);
+				}
+			}
+        return active;
+      }
+    }
+  }
+</script>
